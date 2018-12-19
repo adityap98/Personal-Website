dc10188e185d5831428cae97baaa75895a82d387
Aditya Palacharla <aditya.palacharla@berkeley.edu>
2018-12-19 16:24:29 -0600

    add basic navigation bar UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Button from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <header className="App-header">
           <h1 className="App-title">Welcome to Aditya's Website</h1>
         </header>
